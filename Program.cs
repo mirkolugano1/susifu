26fd38c6f9cb4b1cd0aa30268a6f60b3f5d8afbe
mirkolugano1 <46295846+mirkolugano1@users.noreply.github.com>
2021-07-30 10:08:46 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,51 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 
-namespace ConsoleApp2
+namespace Menuplanung
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var directory = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
-            string[] lines = System.IO.File.ReadAllLines($"{directory}/list.txt");
+            var directory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;            
+            var listOfObjects = File.ReadLines($"{directory}/list.csv").Select(line => new Tuple<string, string>(line.Split(',')[0], String.Join(",", line.Split(',').Skip(1)).Trim('"'))).ToList();
 
-            var entries = lines.Length;
+            var entries = listOfObjects.Count();
             Random r = new Random();
 
             var month = DateTime.Now.ToString("MM");
             var days = 31;
             switch (month)
             {
-                case "02": 
-                    days = 28; 
+                case "02":
+                    days = 28;
                     break;
-                case "04": 
-                case "06": 
-                case "09": 
-                    days = 30; 
+                case "04":
+                case "06":
+                case "09":
+                    days = 30;
                     break;
             }
 
+            var usedCategories = new List<string>();
             var usedEntries = new List<int>();
             var sb = new StringBuilder();
             sb.Append("var menus = {");
             sb.AppendLine();
             for (var i = 0; i < days; i++)
             {
+                if (i % 7 == 0) usedCategories.Clear();
                 int rInt;
                 bool contained;
                 do
                 {
                     rInt = r.Next(0, entries);
                     if (rInt == entries) rInt--;
-                    contained = usedEntries.Contains(rInt);
+                    contained = usedEntries.Contains(rInt) && CountOccurrences(usedCategories, listOfObjects[rInt].Item1) > 1;
                     if (!contained) usedEntries.Add(rInt);
                 } while (contained);
-                var label = lines[rInt];
+
+                var tup = listOfObjects[rInt];
+                usedCategories.Add(tup.Item1);
+                var label = tup.Item2;
                 label = label.Replace("'", "\\'");
                 sb.Append($"{i + 1}: '{label}'");
                 if (i < days - 1) sb.Append(", ");
@@ -53,7 +58,17 @@ namespace ConsoleApp2
             }
             sb.Append("};");
 
-            System.IO.File.WriteAllText($"{directory}/menus.js", sb.ToString());
+            File.WriteAllText($"{directory}/menus.js", sb.ToString());
+        }
+
+        static int CountOccurrences(List<string> usedCategories, string str)
+        {
+            var count = 0;
+            foreach (var cat in usedCategories)
+            {
+                if (cat.Equals(str)) count++;
+            }
+            return count;
         }
     }
 }
