12ffef9ea3e17bb97ee57524134e7a0d5aedb135
mlugano <mirko@beetree.app>
2023-05-12 10:13:16 +0200

    susi2023

--- Webseite/scripts/susi.js
+++ Webseite/scripts/susi.js
@@ -15,14 +15,14 @@ var years = [
     2029,
 ];
 var indexes = {};
-for (var j = 0; j<years.length; j++) {            
+for (var j = 0; j < years.length; j++) {
     indexes[years[j]] = -1;
 }
 var year = years[years.length - 1];
 var isFirstLoad = true;
 var home = '#home';
-window.location.hash = home;        
-        
+window.location.hash = home;
+
 function refreshLayout() {
     if ($('#loading').is(':visible')) {
         var top = ($(window).height() - 60) / 2;
@@ -33,14 +33,19 @@ function refreshLayout() {
     }
 }
 
-$(document).on('ready', function () {            
+var params = {};
+
+$(document).on('ready', function () {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    params = Object.fromEntries(urlSearchParams.entries());
+
     refreshLayout();
     setTimeout(function () {
         $('#loading').hide();
         $('#years').show();
     }, 5000);
 }).on('click', '.yearSelection', function (e) {
-    var value = '#' + $(this).data('value');            
+    var value = '#' + $(this).data('value');
     gotoHash(value);
 });
 
@@ -54,7 +59,7 @@ $(window).on('resize', function () {
             var _year = 0;
             if (_hash != home) _year = parseInt(_hash.substring(1));
             init(_year);
-        } catch (e) {                
+        } catch (e) {
             gotoHash(home);
         }
     }
@@ -67,60 +72,60 @@ var text2016 = 'Cara Susi,#06/06/15 - 06/06/16:#1 anno di noi!!##@##Tra vette (q
     'sei abbagliante come una vittoria lampo in 5 mosse di una partita a scacchi,#' +
     'sei ubriacante come la figura di salsa \'meneala y sacala\',#' +
     'sei gratificante come un birdie in una gara di golf,#' +
-    'sei bella come una sconfitta della juve in finale di Champions.##' + 
+    'sei bella come una sconfitta della juve in finale di Champions.##' +
     'Grazie per il tuo supporto nei momenti difficili.#Grazie per tutti i bei momenti assieme.' +
-    '##£##...a tante nuove avventure insieme...#...e a un nuovo anno di noi!##@##*';            
+    '##£##...a tante nuove avventure insieme...#...e a un nuovo anno di noi!##@##*';
 
 var text2017 = 'Liebe Susi,#06/06/16 - 06/06/17:#Zwei Jahre von uns!!##@##Ein tolles weiteres Jahr mit dir ist nun Geschichte.#' +
-    'Im Gegensatz zum vorherigen Jahr, haben wir in den letzten 12 Monaten nicht viel gereist.#' + 
-    'Allerdings haben wir sehr viel zusammen unternommen:##' + 
+    'Im Gegensatz zum vorherigen Jahr, haben wir in den letzten 12 Monaten nicht viel gereist.#' +
+    'Allerdings haben wir sehr viel zusammen unternommen:##' +
     'Ausflüge mit Freunden...#$#Abenteuer in portugiesischen Inseln...#$#' +
     'wo wir tolle Wanderungen durchgeführt hatten...#$#' +
     'und mit ermutigender Geist, riesige Ungeheuer begegnet und hinter uns gelassen...#$#' +
     'Manchmal bekamen wir Überraschungen auf dem Weg...#$#' +
-    'aber immer mit traumhaften Landschaften im Hintergrund...#$#' + 
-    'Wir jubelten mit den Einheimischen...#$#' + 
-    'teilten aber nicht immer die gleichen Aktivitäten...#$#' + 
+    'aber immer mit traumhaften Landschaften im Hintergrund...#$#' +
+    'Wir jubelten mit den Einheimischen...#$#' +
+    'teilten aber nicht immer die gleichen Aktivitäten...#$#' +
     'Danach widmeten wir uns um etwas kulturell...#$#' +
-    'dann um die Sternen...#$#' + 
-    'Wandern blieb für uns eine tolle Abwechslungsmethode...#$$#' + 
-    'Nachher besuchten wir eine Hochzeit von guten Freunden...#$#' + 
-    'Das Zusammenessen war natürlich ein wichtiger Teil unserer Zeit...#$#' + 
-    'Nach einem kurzen Besuch zu Les Crosets...#$#' + 
+    'dann um die Sternen...#$#' +
+    'Wandern blieb für uns eine tolle Abwechslungsmethode...#$$#' +
+    'Nachher besuchten wir eine Hochzeit von guten Freunden...#$#' +
+    'Das Zusammenessen war natürlich ein wichtiger Teil unserer Zeit...#$#' +
+    'Nach einem kurzen Besuch zu Les Crosets...#$#' +
     'gingen wir zuerst mit Freunden Bowling spielen...#$#' +
-    'dann nach Kloten, um Ambri Piotta zu unterstützen...#$#' + 
-    'immer mit dem Essen im Kopf...#$#' + 
-    'Traurig war unsere schöne Wohnungen belassen zu müssen...#$$#' + 
-    'das aber zum heutigen Zusammenwohnen fuhr...#$#' + 
+    'dann nach Kloten, um Ambri Piotta zu unterstützen...#$#' +
+    'immer mit dem Essen im Kopf...#$#' +
+    'Traurig war unsere schöne Wohnungen belassen zu müssen...#$$#' +
+    'das aber zum heutigen Zusammenwohnen fuhr...#$#' +
     'Eine gute Osterpaella, um wieder zum Thema Essen zurückzukehren...#$#' +
     'Und die letzte (zumindest bis jetzt) schöne spanische Reise...#$#' +
     '##Nee, warte Mal, ich habe etwas vergessen...#' +
     'Eigentlich hatte ich fast das beste des Jahres vergessen...#$#' +
     'Jetzt geht die Rechnung endlich wieder auf!#' +
-    '#Was für ein Jahr ist es gewesen.#' + 
+    '#Was für ein Jahr ist es gewesen.#' +
     'Und was für ein neues Jahr wird das nächste mit dir sein!#' +
-    'Meine Liebe für dich erneut sich jedes Jahr immer wieder, wie eine Niederlage von Le Merde in einem Champions League Finalspiel.#' + 
-    'Ich bedanke mich umso mehr, dass du bei mir bist, und ich bei dir.#' + 
+    'Meine Liebe für dich erneut sich jedes Jahr immer wieder, wie eine Niederlage von Le Merde in einem Champions League Finalspiel.#' +
+    'Ich bedanke mich umso mehr, dass du bei mir bist, und ich bei dir.#' +
     'Ich liebe dich sehr viel.##@##*';
 
 var text2018 = 'Querida Susana##no hay 2 sin 3!!##@##' +
-    'Hoy celebremos el tercer año juntos desde la gita a Colmar#$#' + 
+    'Hoy celebremos el tercer año juntos desde la gita a Colmar#$#' +
     '(y desde la derrota de las mierdas en final)#$#' +
-    'Como los otros años, en este año tambien hemos hecho mucho.#' + 
+    'Como los otros años, en este año tambien hemos hecho mucho.#' +
     'Para bien empezar, hicimos un viaje a lago de Garda#$#' +
     'Ademas algunos paseos como Curzutt#$#' +
     'o el Mythen#$#' +
     'a veces con amigas que no sabian donde ir#$#' +
     'Tambien hicimos gitas en ciudades como Ravensburg#$#' +
-    'Thusis#$#' + 
+    'Thusis#$#' +
     'Biel / Neuchatel#$#' +
     'Bruxelles#$#' +
     'y por dos drogatones como nosotros no podia faltar Amsterdam#$#' +
-    'Otros recuerdos furon un año nuevo con amigos#$#' + 
+    'Otros recuerdos furon un año nuevo con amigos#$#' +
     'y una muy bonita vacaciones de golf en Algarve#$#' +
-    'que fue la cereza sul pastel.##' + 
-    'Algunos se casaron#' + 
-    'tu hermana#$$#' + 
+    'que fue la cereza sul pastel.##' +
+    'Algunos se casaron#' +
+    'tu hermana#$$#' +
     'y algunos amigos como Noemi & Mathias#$#y Sara Fedier#$#' +
     'Si pienso a esto proximo año, estoy seguro que sera asi y tambien muy bonito y interessante como el pasado...#$#' +
     'Te amo muchissimo##*##...cuando menos lo merezca, quiereme.';
@@ -190,9 +195,57 @@ Per la Susi invece, aver chiuso due capitoli del proprio passato #$$#
 ## Ti amo tanto ##@##*
 `;
 
-var text2022 = '';
+var text2023 = `
+Cara Susi##@##
+#$#
+...qua mi pare che anno dopo anno stiamo finendo in secondo piano, visto che qualcun altro ci sta rubando la scena :)#
+Ma torniamo a noi: 8! Otto come le meraviglie del mondo. No, non erano sette. L'ottava sono loro. 
+#$#
+(ecco appunto come dicevo prima...)#
+Senza dimenticare le cose importanti
+#$#
+(e senza dimenticare che a sto giro hanno e avranno pure altro su cui frignare...)#
+e ricordando pure il Doble del Giugno 2022 :) 
+#$#
+Per noi invece tutto va bene, stiamo lentamente ma inesorabilmente uscendo dal tunnel :)#
+E iniziamo a riuscire anche ad andare in vacanza e vivere delle bellissime esperienze come in Sicilia 
+#$$#
+e a Gran Canaria
+#$$$#
+e altre gite varie
+#$$$$#
+e in generale è bello sapere che abbiamo ancora tanti progetti in programma da realizzare (casa, finanze, ditta, ecc.).# 
+## Ti amo tanto ##@##*
+`;
 
-var text2023 = '';
+var text2022 = `
+Cara Susi##@##
+$#$#$#$
+#@@#
+le parole sono superflue qui.#
+A proposito, a sto giro siamo a 7!#
+... 7 come i cieli, #
+... 7 come i nani, #
+...  7 come le vite dei gatti,#
+e ovviamente...#
+... 7 come le finali perse dalle merde :) :)##
+Questo è stato un anno particolare per entrambi, sicuramente molto difficile, con alcune novità sostanziali sia per Jiji
+#$# 
+che per Susi
+#$# 
+La nostra bellissima Martina intanto cresce
+#$# 
+ed è bello vedere come va d'accordo col fratellino #$#$#$#$# 
+e che impara e sa fare sempre nuove cose. #$# 
+Purtroppo non siamo riusciti a viaggiare come gli anni passati per vari motivi, anche Capodanno lo abbiamo festeggiato a casa.#$# 
+Siamo comunque riusciti a farci vacanze low-cost (o meglio: no-cost :) ) in italia
+#$#$#$#$  
+E comunque in generale abbiamo anche passato tanti bei momenti con la nostra bella famiglia
+#$#$#$#$#$ 
+... e pure con la squadra del Jiji, fresca vincitrice del campionato :) #$#
+Ora che inizia anche a migliorare la situazione con i bambini, sono fiducioso e non vedo l'ora di iniziare una nuova annata assieme noi 4 :)
+## Ti amo tanto ##@##*
+`;
 
 var text2024 = '';
 
@@ -243,14 +296,14 @@ var texts = {
 var images = {
     2016: ['sbernardino', 'colmar', 'konstanz', 'spiaggia', 'paella', 'gelatino', 'vars', 'lucca'],
     2017: ['sementina', 'madeira1', 'madeira2', 'madeira3', 'madeira4', 'madeira5', 'madeira6', 'madeira7', 'tosca', 'urania', 'mythen1', 'mythen2', 'hochzeit', 'indiano', 'lescrosets', 'bowling', 'ambri', 'giappo', 'app_jiji', 'app_susi', 'app_schlieren', 'paella_2', 'toledo', 'golf'],
-    2018: ['colmar_1', 'pirlo', 'garda', 'curzutt', 'mythen2017', 'wanderungDaniSusiSabine', 'ravensburg', 'thusis', 'biel', 'bruxelles', 'amsterdam', 'capodanno2018', 'algarve', 'saranico1', 'saranico', 'noemathi', 'sarafedier', 'japan' ],
-    2019: ['juveajax', 'japan2', 'jetdeau', 'walensee', 'montebar', 'hoernli', 'ambrizsc', 'reichenau', 'schaffausen', 'cplsusi2018', 'stoos', 'mallorca', 'algarve2019', 'zermatt', 'casa_urdorf', 'baby_vestitini', 'valencia_restaurant', 'cagatrave_architettura', 'cagatrave_statua',      'cagatrave_orinatoi'],
-    2020: ['m11', 'pietra_ligure', 'finale_ligure', 'zurich_triplete2', 'zurich_triplete1', 'zurich_triplete', 'susisch1', 
+    2018: ['colmar_1', 'pirlo', 'garda', 'curzutt', 'mythen2017', 'wanderungDaniSusiSabine', 'ravensburg', 'thusis', 'biel', 'bruxelles', 'amsterdam', 'capodanno2018', 'algarve', 'saranico1', 'saranico', 'noemathi', 'sarafedier', 'japan'],
+    2019: ['juveajax', 'japan2', 'jetdeau', 'walensee', 'montebar', 'hoernli', 'ambrizsc', 'reichenau', 'schaffausen', 'cplsusi2018', 'stoos', 'mallorca', 'algarve2019', 'zermatt', 'casa_urdorf', 'baby_vestitini', 'valencia_restaurant', 'cagatrave_architettura', 'cagatrave_statua', 'cagatrave_orinatoi'],
+    2020: ['m11', 'pietra_ligure', 'finale_ligure', 'zurich_triplete2', 'zurich_triplete1', 'zurich_triplete', 'susisch1',
         'm1', 'm2', 'm6', 'm4', 'm3', 'm5', 'm7', 'm8', 'm9', 'm10', 'm12', 'm13', 'm14', 'm15', 'm16',
         'malaga_2', 'malaga_1', 'malaga_calcio', 'ronda'],
     2021: [
         '2021_1',
-        'martina_katzensee',        
+        'martina_katzensee',
         '2021_2',
         '2021_3',
         '2021_4',
@@ -280,43 +333,34 @@ var images = {
         '2021_26'
     ],
     2022: [
-        '2022_0',
         '2022_1',
         '2022_2',
+        '2022_14',
+        '2022_16',
+        '2022_21',
+        '2022_20',
         '2022_3',
         '2022_4',
         '2022_5',
-        '2022_6',
-        '2022_7',
-        '2022_8',
-        '2022_9',
         '2022_10',
         '2022_11',
-        '2022_12',
-        '2022_13',
-        '2022_14',
         '2022_15',
-        '2022_16',
+        '2022_7',
         '2022_17',
         '2022_18',
-        '2022_19',
-        '2022_20',
-        '2022_21',
+        '2022_12',
+        '2022_13',
+        '2022_9',
+        '2022_8',
         '2022_22',
         '2022_23',
         '2022_24',
-        '2022_25',
-        '2022_26',
-        '2022_27',
-        '2022_28',
-        '2022_29',
-        '2022_30',
-        '2022_31'
+        '2022_19'
     ],
     2023: [
-        '2023_0',
         '2023_1',
         '2023_2',
+        'pirlo',
         '2023_3',
         '2023_4',
         '2023_5',
@@ -326,26 +370,7 @@ var images = {
         '2023_9',
         '2023_10',
         '2023_11',
-        '2023_12',
-        '2023_13',
-        '2023_14',
-        '2023_15',
-        '2023_16',
-        '2023_17',
-        '2023_18',
-        '2023_19',
-        '2023_20',
-        '2023_21',
-        '2023_22',
-        '2023_23',
-        '2023_24',
-        '2023_25',
-        '2023_26',
-        '2023_27',
-        '2023_28',
-        '2023_29',
-        '2023_30',
-        '2023_31'
+        '2023_12'
     ],
     2024: [
         '2024_0',
@@ -577,7 +602,7 @@ function gotoHash(value) {
     var isChrome = /Chrome/.test(navigator.userAgent);
     var isSafari = /Chrome/.test(navigator.userAgent);
     var isWebkit = isChrome || isSafari;
-    if (location.hash) {                
+    if (location.hash) {
         if (!isWebkit) {
             window.location.hash = value;
         } else {
@@ -586,32 +611,32 @@ function gotoHash(value) {
     }
 };
 
-function init(_year) {   
-    if (!_year || _year == 0) {                                
+function init(_year) {
+    if (!_year || _year == 0) {
         $('#years').show();
         clearTimeout(_timeout);
-                                            
+
         if (!isFirstLoad) {
-            for (var i = 0; i< years.length; i++) {
+            for (var i = 0; i < years.length; i++) {
                 var _y = years[i];
                 $('#myAudio_' + _y)[0].pause();
-                songs[_y].currentTime = $('#myAudio_' + _y)[0].currentTime;        
+                songs[_y].currentTime = $('#myAudio_' + _y)[0].currentTime;
                 $('#container' + _y).hide();
                 $('body').removeClass('background' + _y);
             }
         }
-                                       
+
         refreshLayout();
         return;
-    }            
+    }
     year = _year;
     appendSelector = '#container' + year + ' .mCSB_container';
     $('#years').fadeOut('slow', function () {
         $('body').addClass('background');
         $('body').addClass('background' + year);
-        
+
         $('#myAudio_' + year)[0].currentTime = songs[_year].currentTime;
-        $('#myAudio_' + year)[0].play();                
+        $('#myAudio_' + year)[0].play();
 
         $('#container' + year).fadeIn('slow', function () {
             refreshLayout();
@@ -625,20 +650,22 @@ function init(_year) {
                     updateOnBrowserResize: true,
                     updateOnContentResize: true,
                     autoScrollOnFocus: true
-                }                        
+                }
             });
-            
+
             var _to = indexes[year] < 0 ? 3000 : 0;
-            setTimeout(function(){
+            setTimeout(function () {
                 process(indexes[year] + 1);
-            }, _to);                    
+            }, _to);
         });
-    });                        
+    });
 }
 
 var _timeout;
 
 function doProcess(timeout, i) {
+    if (params.test == 'true') timeout = 10;
+
     $('#container' + year).mCustomScrollbar("scrollTo", 'bottom');
     _timeout = setTimeout(function () {
         process(i + 1);
@@ -650,11 +677,11 @@ function process(i) {
     var text = texts[year];
     if (i > text.length - 1) return;
     var timeout = 80;
-    var letter = text[i];                                           
-    switch (letter) {                
+    var letter = text[i];
+    switch (letter) {
         case '#':
-            $(appendSelector).append('<br/>');      
-            timeout = 1000;              
+            $(appendSelector).append('<br/>');
+            timeout = 1000;
             doProcess(timeout, i);
             break;
         case '£':
@@ -663,7 +690,7 @@ function process(i) {
             $('#customHeart').fadeIn('slow', function () {
                 doProcess(timeout, i);
             });
-            break;                                
+            break;
         case '@':
             var heartContainerId = 'heartContainer_' + heartCount;
             var heartContainer = '<div class="heartContainer hidden" id="' + heartContainerId + '"></div>';
@@ -671,8 +698,8 @@ function process(i) {
             var selector = 'heart_' + heartCount;
             var img = $('<img id="' + selector + '">');
             img.attr('src', 'images/heart.png');
-            img.attr('class', 'heart');                    
-            $('#' + heartContainerId).append(img);                    
+            img.attr('class', 'heart');
+            $('#' + heartContainerId).append(img);
             heartCount++;
             timeout = 1000;
 
@@ -687,8 +714,8 @@ function process(i) {
             img.attr('class', 'picture hidden');
             $(appendSelector).append(img);
             $(appendSelector).append('<div class="clear"></div><br/><br/>');
-            imagesCount[year]++;                    
-            timeout = 5000;                    
+            imagesCount[year]++;
+            timeout = 5000;
             setTimeout(function () {
                 $('#container' + year).mCustomScrollbar("scrollTo", $("#anchor"));
                 setTimeout(function () {
@@ -697,7 +724,7 @@ function process(i) {
                     });
                 }, 500);
             }, 0);
-            
+
             break;
         case '*':
             var div = '<div id="signature">Jiji</div>';
@@ -709,5 +736,5 @@ function process(i) {
             $(appendSelector).append(letter);
             doProcess(timeout, i);
             break;
-    }            
+    }
 }
